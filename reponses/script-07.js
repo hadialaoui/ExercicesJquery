4fc44a641167a2608baadc9a6de334339810e381
hadialaoui <a.hadialaoui@gmail.com>
2017-01-08 23:09:36 +0000

    complete exercice 7

--- reponses/script-07.js
+++ reponses/script-07.js
@@ -1,6 +1,28 @@
 //  Chemin relatif : reponses/script-08.js
 
 
+$(document).on('keydown',function (event) {
+
+    if(event.shiftKey)
+    switch (event.which){
+        case 37 :
+            $(".box").css({left: '-=2'})
+            break
+        case 38 :
+            $(".box").css({top: '-=2'})
+            break
+        case 39 :
+            $(".box").css({left: '+=2'})
+            break
+        case 40 :
+            $(".box").css({ top: '+=2'})
+            break
+
+    }
+
+})
+
+
 
 
 
