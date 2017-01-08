3e1c7e3eef0106dbe0f5346b12c7a7736c288fa0
hadialaoui <a.hadialaoui@gmail.com>
2017-01-08 18:24:39 +0000

    complete exercice6

--- reponses/script-06.js
+++ reponses/script-06.js
@@ -1,11 +1,32 @@
 //  Chemin relatif : reponses/script-06.js
 // String.fromCharCode() permet de récupérer le caractère à partir du code
 
-//Questetion 1 et 2
+//Question 1 et 2
+/*
 var cp=''
 $('#f1').on('keypress',function (event) {
     cp += event.key
     $('#f2').val(cp)
+})
+*/
+//Question 3
+/*
+var cp=''
+$('#f1').on('keypress',function (event) {
+    if(!isNaN(event.key)){
+        cp += event.key
+        $('#f2').val(cp)
+    }
+})
+*/
+//Question 4
+var cp=''
+$('#f1').on('keypress',function (event) {
+    if(!isNaN(event.key)){
+        cp += event.key
+        if(cp.length%3==0)
+        $('#f2').val(cp)
+    }
 })
 
 
